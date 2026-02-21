565294e0085b3bdc98d69ff6d3220c5e027a3212
Nida Arshad <nidaarshadsocials120@gmail.com>
2026-02-20 22:45:32 -0800

    Added StudentDBContext and other real code changes

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,21 @@
-﻿namespace CodefirstEFC.Models
+﻿ using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CodefirstEFC.Models
 {
     public class Student
     {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Column("StudentName", TypeName = "varchar(100)")]
+        [Required] 
+        public string StudentName { get; set; }
+
+
+        [Column("StudentGender", TypeName = "varchar(20)")]
+        public string Gender{ get; set; }
+        [Column("StudentAge")]
+        public int Age { get; set; }
+
     }
 }
